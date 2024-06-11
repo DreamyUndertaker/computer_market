53458a261c0a5e52bed6ebff93fafc5df6c060dd
Dreamy Undertaker <vadiimreg73@gmail.com>
2024-06-11 23:55:42 +0400

    Translated order form to russian language

--- home/forms.py
+++ home/forms.py
@@ -7,4 +7,16 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'address', 'phone']
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'address': 'Адрес',
+            'phone': 'Телефон',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Введите ваше имя'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Введите вашу фамилию'}),
+            'address': forms.TextInput(attrs={'placeholder': 'Введите ваш адрес'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Введите ваш телефон'}),
+        }
 
